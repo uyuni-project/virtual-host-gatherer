20334d2fdd22c969c38d0ed3213b85a1b51a1459
Bo Maryniuk <bo@suse.de>
2015-06-17 18:05:20 +0200

    Lint: add doc string

--- lib/gatherer/modules/__init__.py
+++ lib/gatherer/modules/__init__.py
@@ -12,8 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import abc
+"""
+Contains worker interface.
+"""
 
+import abc
 
 # pylint: disable=abstract-class-not-used
 class WorkerInterface(object):
