uyuni-project/virtual-host-gatherer

commit 3842e4f3530524b18d77e67cbc68a7f53c112e4f

report total CPU numbers in the libvirt module

virtual-host-gatherer/lib/gatherer/modules/Libvirt.py
@@ -222,13 +222,16 @@ class Libvirt(WorkerInterface):
             hypervisor_hostname = conn.getHostname()
             host_capabilities_xml = self.get_host_capabilities(conn)
             host_cpu_topology = self.get_host_cpu_topology(host_capabilities_xml)
+            totalCpuSockets = int(host_cpu_topology.get('sockets'), 1)
+            totalCpuCores = int(host_cpu_topology.get('cores'), 1) * totalSockets
+            totalCpuThreads = int(host_cpu_topology.get('threads'), 1) * totalCores
             output[hypervisor_hostname] = {
                 'name': hypervisor_hostname,
                 'hostIdentifier': host_capabilities_xml.find('host/uuid').text,
                 'type': conn.getType(),
-                'totalCpuSockets': int(host_cpu_topology.get('sockets'), 0),
-                'totalCpuCores': int(host_cpu_topology.get('cores'), 0),
-                'totalCpuThreads': int(host_cpu_topology.get('threads'), 0),
+                'totalCpuSockets': totalCpuSockets,
+                'totalCpuCores': totalCpuCores,
+                'totalCpuThreads': totalCpuThreads,
                 'cpuVendor': host_capabilities_xml.find('host/cpu/vendor').text,
                 'cpuDescription': host_capabilities_xml.find('host/cpu/model').text,
                 'cpuArch': host_capabilities_xml.find('host/cpu/arch').text,

commit 6d8c7b5ebf657da4ac8eba55495860e38ed3d265

Libvirt.py: Fix cpu core handling
Commit report total CPU numbers in the libvirt module
(3842e4f3530524b18d77e67cbc68a7f53c112e4f) has introduced totalCpuSockets,
totalCpuCores, totalCpuThreads variables but used them as totalSockets and
totalCores. Which is wrong and leads to undefined variables.
Testing the commit or just running flake8 would have caught this:

```
$ flake8 Libvirt.py |grep F
Libvirt.py:32:1: F841 local variable 'ex' is assigned to but never used
Libvirt.py:226:70: F821 undefined name 'totalSockets'
Libvirt.py:227:74: F821 undefined name 'totalCores'
```

Moreover, the call to int() is wrong. The python manual says:
```
class int(x=0)
class int(x, base=10)
```
(https://docs.python.org/3/library/functions.html#int)

So, the commit actually tried to convert a string into a number of base 1,
which is wrong.

(has this commit ever tested and properly reviewed ?)

Fixes: 3842e4f3530524b18d77e67cbc68a7f53c112e4f
Signed-off-by: Arnaud Patard <apatard@hupstream.com>

virtual-host-gatherer/lib/gatherer/modules/Libvirt.py
@@ -222,9 +222,9 @@ class Libvirt(WorkerInterface):
             hypervisor_hostname = conn.getHostname()
             host_capabilities_xml = self.get_host_capabilities(conn)
             host_cpu_topology = self.get_host_cpu_topology(host_capabilities_xml)
-            totalCpuSockets = int(host_cpu_topology.get('sockets'), 1)
-            totalCpuCores = int(host_cpu_topology.get('cores'), 1) * totalSockets
-            totalCpuThreads = int(host_cpu_topology.get('threads'), 1) * totalCores
+            totalCpuSockets = int(host_cpu_topology.get('sockets'))
+            totalCpuCores = int(host_cpu_topology.get('cores')) * totalCpuSockets
+            totalCpuThreads = int(host_cpu_topology.get('threads')) * totalCpuCores
             output[hypervisor_hostname] = {
                 'name': hypervisor_hostname,
                 'hostIdentifier': host_capabilities_xml.find('host/uuid').text,