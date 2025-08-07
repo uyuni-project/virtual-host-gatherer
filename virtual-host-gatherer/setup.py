81bf87da6e5fec2207ff81937477cbe39a208597
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2025-08-07 14:04:26 +0100

    Fix format issue on setup.py

--- virtual-host-gatherer/setup.py
+++ virtual-host-gatherer/setup.py
@@ -2,6 +2,8 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+# pylint: disable=missing-module-docstring,deprecated-module
+
 #!/usr/bin/python3
 #
 #
@@ -9,16 +11,18 @@
 from __future__ import absolute_import
 from distutils.core import setup
 
-setup(name="virtual-host-gatherer",
-      version="1.0.28",
-      description="Gather virtual host and VM data",
-      long_description="""\
+setup(
+    name="virtual-host-gatherer",
+    version="1.0.28",
+    description="Gather virtual host and VM data",
+    long_description="""\
 Gather virtual host and VM data from different kind of hypervisors
 """,
-      author='Michael Calmer',
-      author_email='mc@suse.com',
-      url='http://www.suse.com',
-      package_dir={'': 'lib'},
-      packages=['gatherer', 'gatherer/modules'],
-      scripts=['scripts/virtual-host-gatherer'],
-      license="Apache-2.0",)
+    author="Michael Calmer",
+    author_email="mc@suse.com",
+    url="http://www.suse.com",
+    package_dir={"": "lib"},
+    packages=["gatherer", "gatherer/modules"],
+    scripts=["scripts/virtual-host-gatherer"],
+    license="Apache-2.0",
+)
