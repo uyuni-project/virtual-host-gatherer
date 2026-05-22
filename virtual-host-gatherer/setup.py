6bc01c0015e4367677f841e44a24488dd5beee15
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2026-05-22 10:58:43 +0100

    Automatic commit of package [virtual-host-gatherer] release [1.0.31-0].

--- virtual-host-gatherer/setup.py
+++ virtual-host-gatherer/setup.py
@@ -11,7 +11,7 @@ from distutils.core import setup
 
 setup(
     name="virtual-host-gatherer",
-    version="1.0.30",
+    version="1.0.31",
     description="Gather virtual host and VM data",
     long_description="""\
 Gather virtual host and VM data from different kind of hypervisors
